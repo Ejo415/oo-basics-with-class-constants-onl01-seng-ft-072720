61085673d8200793d897331faf3db77efbd94e26
Eugene O'Neil <ejo7220@gmail.com>
2020-08-05 18:06:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |brand|
-      BRANDS << brand unless uniques.include?(x)
+    if !(BRANDS.include?(@brand))
+      BRANDS << brand
+      
       
     end 
    
